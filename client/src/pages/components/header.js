0a74e56d272f3439ff700f74f9b2c395571ca1c3
AJoelj84 <ajoelj84@gmail.com>
2023-07-06 18:58:37 -0400

    made changes to header.js to more resemble a navbar

--- client/src/pages/components/header.js
+++ client/src/pages/components/header.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import Auth from '../../utils/auth';
 
 const Header = () => {
+  const handleLogout = () => {
+    Auth.logout();
+  };
+
   return (
     <header>
-      {/* Header content */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          {Auth.loggedIn() ? (
+            <>
+              <li>
+                <button onClick={handleLogout}>Logout</button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signup">Signup</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
     </header>
   );
 };
 
 export default Header;
+
 
 
 
