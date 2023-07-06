9ab3ca7f014052f0cb2b948d3dad4a5a9a7cd192
peejd <121059860+peejd@users.noreply.github.com>
2023-07-06 19:57:39 -0400

    added typedefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,50 +1,63 @@
 const { gql } = require('apollo-server-express');
 
-// const typeDefs = gql`
-//   type User {
-//     _id: ID
-//     username: String
-//     email: String
-//     password: String
-//     thoughts: [Thought]!
-//   }
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    zipcode: Number
+    isArtist: Boolean
+    contact: STring
+    imageURL: String
+    posts: [Post!]
+    createdAt: String!
+    updatedAt: String!
+  }
 
-//   type Thought {
-//     _id: ID
-//     thoughtText: String
-//     thoughtAuthor: String
-//     createdAt: String
-//     comments: [Comment]!
-//   }
+  type Post {
+    _id: ID
+    postText: String!
+    imageURL: String
+    username: String!
+    createdAt: String!
+    updatedAt: String!
+    comments: [Comment!]
+  }
 
-//   type Comment {
-//     _id: ID
-//     commentText: String
-//     commentAuthor: String
-//     createdAt: String
-//   }
+  type Comment {
+    commentId: ID
+    commentText: String!
+    username: String!
+    createdAt: String!
+  }
 
-//   type Auth {
-//     token: ID!
-//     user: User
-//   }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-//   type Query {
-//     users: [User]
-//     user(username: String!): User
-//     thoughts(username: String): [Thought]
-//     thought(thoughtId: ID!): Thought
-//     me: User
-//   }
+  type Query {
+    users: [User]
+    user(username: String!): User
+    posts: [Post]
+    userPosts(username: String!): [Post]
+    post(postId: ID!): Post
+    comments(postId: ID!): [Comment]
+    me: User
+  }
 
-//   type Mutation {
-//     addUser(username: String!, email: String!, password: String!): Auth
-//     login(email: String!, password: String!): Auth
-//     addThought(thoughtText: String!): Thought
-//     addComment(thoughtId: ID!, commentText: String!): Thought
-//     removeThought(thoughtId: ID!): Thought
-//     removeComment(thoughtId: ID!, commentId: ID!): Thought
-//   }
-// `;
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!, zipcode: Int, isArtist: Boolean!, contact: String, imageURL: String): Auth
+    login(email: String!, password: String!): Auth
+    updateUser(zipcode: Int, isArtist: Boolean!, contact: String, imageURL: String): User
+    addPost(postText: String!, imageURL: String): Post
+    updatePost(postId: ID!, postText: String, imageURL: String): Post
+    removePost(postId: ID!): Post
+    addComment(postId: ID!, commentText: String!): Comment
+    updateComment(postId: ID!, commentId: ID!, commentText: String!): Comment
+    removeComment(postId: ID!, commentId: ID!): Comment
+  }
+`;
 
 module.exports = typeDefs;
