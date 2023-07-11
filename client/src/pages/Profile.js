fe3238411672647b50a29ae5a3db092d1bf8ec78
AJoelj84 <ajoelj84@gmail.com>
2023-07-11 19:53:14 -0400

    auth and profile working

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -26,7 +26,7 @@ const Profile = () => {
     return <div>Loading...</div>;
   }
 
-  if (!user?.username) {
+  if (user.username) {
     return (
       <h4>
         You need to be logged in to see this. Use the navigation links above to sign up or log in!
