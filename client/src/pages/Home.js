426c645ccc5e47f62094e9e5a73dc70f54dbcd03
Hayden Caffey <hayden@Haydens-MacBook-Pro-2.local>
2023-07-11 01:42:50 -0400

    style updates and placeholder body stuff- needs verify

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,11 +5,26 @@ const Home = () => {
 
 
   return (
-    <div>
-      Hello, This is the Body in the HOME section, Home should have Post Lists, and endlesss scroll
-      Any Side Bar elements That may be needed.
-    </div>
+    <div className="d-flex justify-content-center rounded">
+        {<div class="d-flex card w-50">
+          <img class="rounded pb-1" alt="image of tattoo" src="https://www.itattoodesigns.com/images/articles/rat_tattoo_designs.jpg"></img>
+            <div class="card-body">
+              <h5 class="card-title">Need to link post title, and iterate through posts, check comments on Home.js</h5>
+              <p class="card-text">Example caption... NOTE image is hard coded at current</p>
+              <a href="#" class="btn  btn-primary">Link to comment entry</a>
+            </div>
+          </div> 
+        }
+      </div>
   );
 };
 
 export default Home;
+
+
+   /* <PostStuff
+            imageURL={post.imageURL}
+            postText={`${post.postText}`}
+            showTitle={false}
+            showUsername={false}
+          /> */
