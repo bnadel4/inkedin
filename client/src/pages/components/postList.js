bdb7471fcbb0d68f0235ec68207665074c63b1dc
AJoelj84 <ajoelj84@gmail.com>
2023-07-11 11:23:35 -0400

    styling changes to postList

--- client/src/pages/components/postList.js
+++ client/src/pages/components/postList.js
@@ -1,8 +1,14 @@
 import React from 'react';
+
 import CommentForm from './commentForm';
+
 import { useQuery } from '@apollo/client';
 import { GET_POSTS } from '../../utils/queries';
+
 import '../css/style.css';
+
+
+
 const PostList = () => {
   // Use the useQuery hook to fetch the posts
   const { loading, error, data } = useQuery(GET_POSTS);
@@ -16,17 +22,16 @@ const PostList = () => {
   return (
     <div>
       { !loading && posts && posts.map((post) => (
-        <div class="card container col-md-5 border border-5 border-dark postlist posttext" key={post._id}>
+        <div class="card container col-md-5 border border-5 border-dark rounded-5 postlist posttext" key={post._id}>
           <h3 class="card-title">{post.username}</h3>
-          <p>{post.postText}</p>
-          {post.imageURL && <img src={post.imageURL} alt="Post" />}
+            <p>{post.postText}</p>
+              {post.imageURL && <img src={post.imageURL} alt="Post" />}
           <p><CommentForm/></p>
-          <h4 class="card-text">Comments:</h4>
-          {post.comments.map((comment) => (
-            <div class="card-footer text-muted" key={comment._id}>
-              <p>{comment.username}: {comment.commentText}</p>
-
-            </div>
+            <h4 class="card-text">Comments:</h4>
+              {post.comments.map((comment) => (
+              <div class="card-footer text-muted" key={comment._id}>
+                <p>{comment.username}: {comment.commentText}</p>
+              </div>
           ))}
         </div>
       ))}
