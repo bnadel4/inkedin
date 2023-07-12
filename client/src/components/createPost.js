b90430db83f9aeecd26e71765cfb5282fa419c31
peejd <121059860+peejd@users.noreply.github.com>
2023-07-11 21:47:37 -0400

    got ADD_USER mutation working for the createPost.js page

--- client/src/components/createPost.js
+++ client/src/components/createPost.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 
-import { ADD_THOUGHT } from '../../utils/mutations';
-import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
+import { ADD_POST } from '../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../utils/queries';
 
 import Auth from '../utils/auth';
 
