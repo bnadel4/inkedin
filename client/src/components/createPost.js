41160a75bcecfe8dc9d82fca2861198924f4b66a
AJoelj84 <ajoelj84@gmail.com>
2023-07-12 14:39:47 -0400

    Changes to the Profile Page

--- client/src/components/createPost.js
+++ client/src/components/createPost.js
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
-
-import { ADD_THOUGHT } from '../../utils/mutations';
-import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
-
+import { ADD_THOUGHT } from '../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
-
-// need to add back in all the Auth and Mutations above into the code below
 
 const CreatePost = ({ onSubmit }) => {
   const [title, setTitle] = useState('');
@@ -22,130 +18,36 @@ const CreatePost = ({ onSubmit }) => {
   };
 
   return (
-    <div>
-      <h2>Create a New Post</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Title"
-          required
-        />
-        <textarea
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          placeholder="Content"
-          required
-        ></textarea>
-        <button type="submit">Submit</button>
-      </form>
+    <div className="card">
+      <div className="card-body">
+        <h2>Create a New Post</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Title"
+              required
+            />
+            <label htmlFor="title">Title</label>
+          </div>
+          <div className="form-floating mb-3">
+            <textarea
+              className="form-control"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              placeholder="Content"
+              required
+            ></textarea>
+            <label htmlFor="content">Content</label>
+          </div>
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
 
 export default CreatePost;
-
-
-// const ThoughtForm = () => {
-//   const [thoughtText, setThoughtText] = useState('');
-
-//   const [characterCount, setCharacterCount] = useState(0);
-
-//   const [addThought, { error }] = useMutation(ADD_THOUGHT, {
-//     update(cache, { data: { addThought } }) {
-//       try {
-//         const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
-
-//         cache.writeQuery({
-//           query: QUERY_THOUGHTS,
-//           data: { thoughts: [addThought, ...thoughts] },
-//         });
-//       } catch (e) {
-//         console.error(e);
-//       }
-
-//       // update me object's cache
-//       const { me } = cache.readQuery({ query: QUERY_ME });
-//       cache.writeQuery({
-//         query: QUERY_ME,
-//         data: { me: { ...me, thoughts: [...me.thoughts, addThought] } },
-//       });
-//     },
-//   });
-
-//   const handleFormSubmit = async (event) => {
-//     event.preventDefault();
-
-//     try {
-//       const { data } = await addThought({
-//         variables: {
-//           thoughtText,
-//           thoughtAuthor: Auth.getProfile().data.username,
-//         },
-//       });
-
-//       setThoughtText('');
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
-
-//   const handleChange = (event) => {
-//     const { name, value } = event.target;
-
-//     if (name === 'thoughtText' && value.length <= 280) {
-//       setThoughtText(value);
-//       setCharacterCount(value.length);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h3>What's on your techy mind?</h3>
-
-//       {Auth.loggedIn() ? (
-//         <>
-//           <p
-//             className={`m-0 ${
-//               characterCount === 280 || error ? 'text-danger' : ''
-//             }`}
-//           >
-//             Character Count: {characterCount}/280
-//           </p>
-//           <form
-//             className="flex-row justify-center justify-space-between-md align-center"
-//             onSubmit={handleFormSubmit}
-//           >
-//             <div className="col-12 col-lg-9">
-//               <textarea
-//                 name="thoughtText"
-//                 placeholder="Here's a new thought..."
-//                 value={thoughtText}
-//                 className="form-input w-100"
-//                 style={{ lineHeight: '1.5', resize: 'vertical' }}
-//                 onChange={handleChange}
-//               ></textarea>
-//             </div>
-
-//             <div className="col-12 col-lg-3">
-//               <button className="btn btn-primary btn-block py-3" type="submit">
-//                 Add Thought
-//               </button>
-//             </div>
-//             {error && (
-//               <div className="col-12 my-3 bg-danger text-white p-3">
-//                 {error.message}
-//               </div>
-//             )}
-//           </form>
-//         </>
-//       ) : (
-//         <p>
-//           You need to be logged in to share your thoughts. Please{' '}
-//           <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
-//         </p>
-//       )}
-//     </div>
-//   );
-// };
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
-import './css/style.css'
 
 const Login = () => {
   const [username, setUsername] = useState('');
@@ -29,40 +29,42 @@ const Login = () => {
 
   return (
     <div className="d-flex justify-content-center align-items-center loginform">
-      <div>
-        <h2 className='mb-4'>Login</h2>
-        <form onSubmit={handleLogin}>
-          <div className="form-floating mb-3">
-            <input
-              type="email"
-              className="form-control"
-              id="floatingInput"
-              placeholder="name@example.com"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <label htmlFor="floatingInput">Email address</label>
-          </div>
-          <div className="form-floating">
-            <input
-              type="password"
-              className="form-control"
-              id="floatingPassword"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <label htmlFor="floatingPassword">Password</label>
-          </div>
-          <button type="submit" className="btn btn-lg btn-secondary mt-4">Login</button>
-        </form>
+      <div className="card">
+        <h4 className="card-header bg-dark text-light p-2">Login</h4>
+        <div className="card-body">
+          <form onSubmit={handleLogin}>
+            <div className="form-floating mb-3">
+              <input
+                type="email"
+                className="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <label htmlFor="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating">
+              <input
+                type="password"
+                className="form-control"
+                id="floatingPassword"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <label htmlFor="floatingPassword">Password</label>
+            </div>
+            <button type="submit" className="btn btn-lg btn-secondary mt-4">Login</button>
+          </form>
 
-        {error && (
-          <div>
-            <p>Error logging in. Please try again.</p>
-            <p>{error.message}</p>
-          </div>
-        )}
+          {error && (
+            <div>
+              <p>Error logging in. Please try again.</p>
+              <p>{error.message}</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
